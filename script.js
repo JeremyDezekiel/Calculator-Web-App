66244a9a110f40f6c16b5c6ae732ba9d1175ae64
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-11-07 22:03:42 +0700

    add function to input button and show on screen

---
+++ script.js
@@ -0,0 +1,69 @@
+const btnOne = document.querySelector('#btnOne')
+const screen = document.querySelector('.screen')
+const btnTwo = document.querySelector('#btnTwo')
+
+// btnOne.addEventListener('click', function(){
+//     screen.innerText = '1';
+// })
+
+// btnOne.addEventListener('click', function(){
+//     if (screen.innerText == '0') {
+//         screen.innerText = '1'
+//     } else {
+//         screen.innerText = screen.innerText + '1'
+//     }
+// })
+
+btnOne.addEventListener('click', function(){
+    if (screen.innerText == '0') {
+        screen.innerText = '1'
+    } else {
+        screen.innerText += '1'
+    }
+})
+
+btnTwo.addEventListener('click', function(){
+    if (screen.innerText == '0') {
+        screen.innerText = '2'
+    } else {
+        screen.innerText += '2'
+    }
+})
+
+// function btnSymbol(symbol) {
+//     if (screen.innerText == '0') {
+//         screen.innerText = symbol
+//     } else {
+//         screen.innerText += symbol
+//     }
+// }
+
+function btnSymbol(symbol) {
+    if (screen.innerText == '0') {
+        screen.innerText = symbol
+    } else {
+        screen.innerText += symbol
+    }
+}
+
+function button(num) {
+    if (screen.innerText == '0') {
+        screen.innerText = num
+    } else {
+        screen.innerText += num
+    }
+}
+
+function AC() {
+    screen.innerText = ''
+}
+
+// function Del() {
+//     if (screen.innerText == num) {
+//         screen.innerText = screen.innerText.toString().slice (0,-1)
+//     }
+// }
+
+function equal() {
+    screen.innerText = eval(screen.innerText)
+}
