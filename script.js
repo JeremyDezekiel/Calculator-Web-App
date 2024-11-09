50ba37adb5b8b4b87b9916d537e326781747e44e
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-11-09 17:52:10 +0700

    repair btnSymbol, btnPoint, btnNumb, btnZero

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function btnSymbol(symbol) {
     
     if (screen.value == '') {
         screen.value = ''
+    } else if (result.value == 'NaN') {
+        screen.value = screen.value.slice(0, -2) + symbol
+        result.value = '0'
+    } else if (result.value == 'Infinity') {
+        screen.value = screen.value.slice(0, -2) + symbol
+        result.value = '0'
     } else if (lastChar == '+' || lastChar == '-' || lastChar == '%' || lastChar == '/' || lastChar == '*') {
         screen.value = screen.value.slice(0, -1) + symbol
     } else if (result.value != '0') {
@@ -19,13 +25,20 @@ function btnPoint(point) {
     const lastChar = screen.value.slice(-1)
     if (screen.value == '') {
         screen.value = '0' + point
+    } else if (result.value == 'Infinity') {
+        screen.value += point
+        result.value = '0'
+    } else if (result.value == 'NaN') {
+        screen.value += point
+        result.value = '0'
+    } else if (lastChar == '+' || lastChar == '-' || lastChar == '%' || lastChar == '/' || lastChar == '*') {
+        screen.value = screen.value
     } else if (lastChar != point)
         screen.value += point
 }
 
 function btnPlusMinus() {
     const firstChar = screen.value.slice(0, 1)
-    console.log(firstChar)
     if (firstChar != '-') {
         screen.value = '-' + screen.value
     } else
@@ -37,6 +50,21 @@ function btnNumb(num) {
     const sLastChar = screen.value.slice(-2, -1)
     if (screen.value == '0') {
         screen.value = num
+    } else if (result.value == 'ERROR') {
+        screen.value = screen.value + num
+        result.value = '0'
+    } else if (result.value == 'Infinity' && lastChar == '0') {
+        screen.value = screen.value.slice(0, -1) + num
+        result.value = '0'
+    } else if (result.value == 'Infinity' && lastChar == '.') {
+        screen.value += num
+        result.value = '0'
+    } else if (result.value == 'NaN' && lastChar == '0') {
+        screen.value = screen.value.slice(0, -1) + num
+        result.value = '0'
+    } else if (result.value == 'NaN' && lastChar == '.') {
+        screen.value += num
+        result.value = '0'
     } else if (lastChar == '0' && sLastChar == '+') {
         screen.value = screen.value.slice(0, -1) + num
     } else if (lastChar == '0' && sLastChar == '-') {
@@ -47,6 +75,9 @@ function btnNumb(num) {
         screen.value = screen.value.slice(0, -1) + num
     } else if (lastChar == '0' && sLastChar == '*') {
         screen.value = screen.value.slice(0, -1) + num
+    } else if (result.value != '0') {
+        result.value = '0'
+        screen.value = num
     } else {
         screen.value += num
     }
@@ -57,6 +88,9 @@ function btnZero(nol) {
     const sLastChar = screen.value.slice(-2, -1)
     if (screen.value == '0') {
         screen.value = nol
+    } else if (result.value == 'ERROR') {
+        screen.value += nol
+        result.value = '0'
     } else if (lastChar == '0' && sLastChar == '+') {
         screen.value = screen.value.slice(0, -1) + '0'
     } else if (lastChar == '0' && sLastChar == '-') {
@@ -84,10 +118,13 @@ function Del() {
 
 function equal() {
     const lastChar = screen.value.slice(-1)
+    const sLastChar = screen.value.slice (-2,-1)
     if (screen.value == '') {
         screen.value = ''
     } else if (lastChar == '+' || lastChar == '-' || lastChar == '%' || lastChar == '/' || lastChar == '*') {
         result.value = 'ERROR'
+    } else if ( lastChar == '0' && sLastChar == '/') {
+        result.value = 'Infinity'
     } else {
         result.innerText = eval(screen.value)
     }
